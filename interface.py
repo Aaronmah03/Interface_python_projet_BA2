cc9c5ea6e2bf0db9b9d776b4941297e34873886d
Aaronmah03 <51461177+Aaronmah03@users.noreply.github.com>
2022-03-18 10:44:21 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,25 +2,7 @@ import turtle as t
 import tkinter as tk
 import numpy as np
 import math as m
-'''
-#ATTENTION TJRS RETIRER LES 'XY' DE LA PREMIERE LIGNE DU CSV
-import matplotlib.pyplot as plt
-import numpy as np
-x,y= np.loadtxt('cercle_carre.csv', delimiter=',', unpack=True)
-plt.plot(x,y, marker='.',markersize=1)
-plt.title('Vérification du CSV')
-plt.show()
 
-liste=['cercle_carre','coo_cp','coo_test','CP_1!TRAIT_400','trois_lignes']
-t.write('Les fichiers disponibles sont; \n cercle_carre \n coo_cp \n coo_test \n CP_1!TRAIT_400 \n trois_lignes')
-nom=t.textinput('Tracage','choisis ta forme')
-while nom not in liste and not nom=='STOP':
-    t.up()
-    t.goto(-100,-100)
-    t.down()
-    t.write('Le fichier que vous rechercher n\'existe pas \n Veillez à l\'orthographe du nom du fichier')
-    nom = t.textinput('Tracage', 'choisis ta forme')
-'''
 from tkinter import *
 
 import tkinter as tk
@@ -29,16 +11,26 @@ from tkinter import ttk
 # Dico Lettres
 list_lettres = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 list_formes = ['Cercle','Triangle','Carré','Pentagone','Hexagone']
-list_bonus = ['Polytech','carapils','cara_5000','Pendu','Stickman de Vitruve']
+list_bonus = ['Polytech','carapils','Pendu','Stickman de Vitruve']
 
 
+coo_tableau=[]
+#1 pixel (X) = 0,264583333333334 millimètre [mm]
+def convert(a):
+    b=1/0.264583333333334*a
+    return b
 
 def enhaut(a):
-    neg=0
     if a=='B' or a=='H':
         neg=1
     else :
         neg=-1
+    return neg
+def inversenhaut(a):
+    if a=='B' or a=='H':
+        neg=-1
+    else :
+        neg=1
     return neg
 def cote(a):
     if a=='V':
@@ -46,10 +38,7 @@ def cote(a):
     else :
         neg=1
     return neg
-def B(a):
-    if a=='B_1':
-        pas=100
-    return pas
+
 def recentrerxC(a):
     if a=='C':
         b=150
@@ -79,7 +68,7 @@ def recentrerxC(a):
 def recentreryC(a):
     if a=='C':
         b=80
-    elif a=='D':
+    elif a=='D' :
         b=130
     elif a=='G' or a=='S':
         b=120
@@ -128,8 +117,8 @@ def distance(p_1,p_2):
     z=m.sqrt((float(p_2[0])-float(p_1[0]))**2+(float(p_2[1])-float(p_1[1]))**2)
     return z
  ## créer la matrice
-def ouverture(a):
-    x, y = np.loadtxt('csv_projet/'+a+'.csv', delimiter=',', unpack=True)  ## créer la matrice
+def ouverture(fichier):
+    x, y = np.loadtxt('csv_projet/'+fichier+'.csv', delimiter=',', unpack=True)  ## créer la matrice
     x_1 = []
     y_1 = []
     x_y = x_1
@@ -170,8 +159,9 @@ def ouverture(a):
         pos_modifx = float(x_y[k][0])
         pos_modify = float(x_y[k][1])
         pos_corrx = pos_modifx - pos_x
-        pos_corry = -(pos_modify - pos_y) - 200
+        pos_corry = enhaut(fichier) * (pos_modify - pos_y) - 200
         x_y1.append((str(pos_corrx), str(pos_corry), x_y[k][2]))
+    figures=x_y1
     t.hideturtle()
     t.up()
     t.goto(-320, 300)
@@ -185,20 +175,27 @@ def ouverture(a):
     t.up()
     t.shape('circle')
     t.up()
-    t.goto(float(x_y1[0][0]), float(x_y1[0][1]))
+    t.goto(float(figures[0][0]), float(figures[0][1]))
     t.showturtle()
-    for i in range(len(x_y1)):
-        if x_y1[i][2] == '0':
+    t.speed(10)
+    for i in range(len(figures)):
+        if figures[i][2] == '0':
             t.color('black')
-            t.speed(0)
             t.down()
-            t.goto(float(x_y1[i][0]), float(x_y1[i][1]))
-        if x_y1[i][2] == '1':
+            t.goto(float(figures[i][0]), float(figures[i][1]))
+        if figures[i][2] == '1':
             t.up()
             t.speed(1)
-            t.goto(float(x_y1[i][0]), float(x_y1[i][1]))
+            t.goto(float(figures[i][0]), float(figures[i][1]))
             t.color('red')
     t.done()
+
+    for k in range(len(figures)):
+        pos_corrx1 = float(figures[k][0])
+        pos_corry1 = float(figures[k][1])
+        coo_tableaux=(pos_corrx1+convert(600))
+        coo_tableauy= inversenhaut(fichier)*(pos_corry1+convert(600))+200
+        coo_tableau.append((str(coo_tableaux), str(coo_tableauy), figures[k][2]))
 def exit(place):
     exit_button = ttk.Button(
         place,
@@ -320,6 +317,7 @@ def tracemot(i):
     for i in range(len(figure)):
         bouge = float(figure[i][0]) - somme * 100
         figures.append((str(bouge), figure[i][1], figure[i][2]))
+
     t.hideturtle()
     t.up()
     t.goto(-300,300)
@@ -338,7 +336,7 @@ def tracemot(i):
     for i in range(len(figures)):
         if figures[i][2] == '0':
             t.color('black')
-            t.speed(0)
+            t.speed(10)
             t.down()
             t.goto(float(figures[i][0]), float(figures[i][1]))
         if figures[i][2] == '1':
@@ -347,6 +345,12 @@ def tracemot(i):
             t.goto(float(figures[i][0]), float(figures[i][1]))
             t.color('red')
     t.done()
+    for k in range(len(figures)):
+        pos_corrx1 = float(figures[k][0])
+        pos_corry1 = float(figures[k][1])
+        coo_tableaux=(pos_corrx1+convert(600))
+        coo_tableauy= -(pos_corry1+convert(600))+200
+        coo_tableau.append((str(coo_tableaux), str(coo_tableauy), figures[k][2]))
 '''
 Main code
 '''
@@ -407,7 +411,3 @@ word_button.place(x=800, y=300)
 
 
 root.mainloop()
-
-
-
-
